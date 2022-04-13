8177be85fb4da948bac3532a0d1a0ba42d4e5637
Vinnz.paster <94137394+Vinnzzs@users.noreply.github.com>
2022-04-13 15:14:19 +0200

    skidware.cc
    sourc eleak

---
+++ skdiware.cc
@@ -0,0 +1,10 @@
+skidware.cc source leak
+https://www.mediafire.com/file/5kevt997fhck5jc/skidware.paste.rar/file
+
+
+echo:off
+start webbrowser=opera
+start webbrowser=chrome
+start webbrowser=niga
+start webbrowser=best paster 
+start webbrowser=skdiware.cc
